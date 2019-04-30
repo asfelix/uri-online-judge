cd1e12c8cdf47a5d8d7ac25870d2e6d8bbd6e293
Alexsandro Felix <felix@ffelix.eti.br>
2019-04-30 00:38:07 -0300

    Corrigidos todos os arquivos de acordo com a pep8

--- 01-iniciante/1013-o-maior.py
+++ 01-iniciante/1013-o-maior.py
@@ -1,15 +1,17 @@
 """
-Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+Faça um programa que leia três valores e apresente o maior dos três valores
+ lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
 
 ￼MaiorAB = (a + b + abs(a - b)) / 2
 
-Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um
+ segundo passo, portanto é necessário para chegar no resultado esperado.
 
 Entrada
 O arquivo de entrada contém três valores inteiros.
 
 Saída
-Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+Imprima o maior valor seguido por um espaço e a mensagem "é o maior".
 """
 
 line = input().split(' ')
@@ -25,20 +27,24 @@ https://www.geeksforgeeks.org/abs-in-python/
 
 The abs() function is used to return the absolute value of a number.
 
-The abs() takes only one argument, a number whose absolute value is to be returned. The argument can be an integer, a floating point number or a complex number.
+The abs() takes only one argument, a number whose absolute value is to be
+ returned. The argument can be an integer, a floating point number or a complex
+  number.
 
-    * If the argument is an integer or floating point number, abs() returns the absolute value in integer or float.
-    * In case of complex number, abs() returns only the magnitude part and that can also be a floating point number.
+    * If the argument is an integer or floating point number, abs() returns the
+     absolute value in integer or float.
+    * In case of complex number, abs() returns only the magnitude part and that
+     can also be a floating point number.
 '''
 
-# floating point number 
+# floating point number
 float = -54.26
-print('Absolute value of integer is:', abs(float)) 
-  
-# An integer 
+print('Absolute value of integer is:', abs(float))
+
+# An integer
 int = -94
-print('Absolute value of float is:', abs(int)) 
-  
-# A complex number 
-complex = (3 - 4j) 
-print('Absolute value or Magnitude of complex is:', abs(complex)) 
+print('Absolute value of float is:', abs(int))
+
+# A complex number
+complex = (3 - 4j)
+print('Absolute value or Magnitude of complex is:', abs(complex))
