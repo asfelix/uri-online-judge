e0d632a1df70ad84d06256ba4263b721dfaf8695
Alexsandro Felix <felix@ffelix.eti.br>
2019-04-29 23:33:32 -0300

    Exercícios resolvidos em 29/04/2019

---
+++ 01-iniciante/1013-o-maior.py
@@ -0,0 +1,44 @@
+'''
+Faça um programa que leia três valores e apresente o maior dos três valores lidos seguido da mensagem “eh o maior”. Utilize a fórmula:
+
+￼MaiorAB = (a + b + abs(a - b)) / 2
+
+Obs.: a fórmula apenas calcula o maior entre os dois primeiros (a e b). Um segundo passo, portanto é necessário para chegar no resultado esperado.
+
+Entrada
+O arquivo de entrada contém três valores inteiros.
+
+Saída
+Imprima o maior dos três valores seguido por um espaço e a mensagem "eh o maior".
+'''
+
+line = input().split(' ')
+A, B, C = int(line[0]), int(line[1]), int(line[2])
+
+maiorAB = (A + B + abs(A - B)) / 2
+maior = (maiorAB + C + abs(maiorAB - C)) / 2
+
+print('{:.0f} é o maior' .format(maior))
+
+'''
+https://www.geeksforgeeks.org/abs-in-python/
+
+The abs() function is used to return the absolute value of a number.
+
+The abs() takes only one argument, a number whose absolute value is to be returned. The argument can be an integer, a floating point number or a complex number.
+
+    * If the argument is an integer or floating point number, abs() returns the absolute value in integer or float.
+    * In case of complex number, abs() returns only the magnitude part and that can also be a floating point number.
+'''
+
+# floating point number 
+float = -54.26
+print('Absolute value of integer is:', abs(float)) 
+  
+# An integer 
+int = -94
+print('Absolute value of float is:', abs(int)) 
+  
+# A complex number 
+complex = (3 - 4j) 
+print('Absolute value or Magnitude of complex is:', abs(complex)) 
---
+++ 01-iniciante/1015-distancia-entre-dois-pontos.py
@@ -0,0 +1,23 @@
+'''
+Leia os quatro valores correspondentes aos eixos x e y de dois pontos quaisquer no plano, p1(x1,y1) e p2(x2,y2) e calcule a distância entre eles, mostrando 4 casas decimais após a vírgula, segundo a fórmula:
+
+Distancia =￼ square((x2 - x1) ** 2) + (y2 - y1) ** 2))
+
+Entrada
+O arquivo de entrada contém duas linhas de dados. A primeira linha contém dois valores de ponto flutuante: x1 y1 e a segunda linha contém dois valores de ponto flutuante x2 y2.
+
+Saída
+Calcule e imprima o valor da distância segundo a fórmula fornecida, com 4 casas após o ponto decimal.
+'''
+
+from math import sqrt
+
+line1 = input().split(' ')
+line2 = input().split(' ')
+
+p1 = float(line1[0]), float(line1[1])
+p2 = float(line2[0]), float(line2[1])
+
+distancia = sqrt(((p2[0] - p1[0]) ** 2) + ((p2[1] - p1[1]) ** 2))
+
+print('{:.4f}' .format(distancia))
