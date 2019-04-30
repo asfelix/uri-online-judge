e173312f48ddbd267708be6b476512996c2c119f
Alexsandro Felix <felix@ffelix.eti.br>
2019-04-30 08:37:10 -0300

    Fixando um pouco mais * 'parte inteira da divisão' //   199 // 100 = 1 * 'módulo, ou resto da divisão' %   199 % 100 = 99
    link de referência:
    http://excript.com/python/operador-aritmetico-em-python.html

---
+++ 01-iniciante/1019-conversao-de-tempo.py
@@ -0,0 +1,16 @@
+"""
+Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N.
+
+Saída
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido.
+"""
+t = int(input())
+h = t // 3600
+m = t % 3600
+m = m // 60
+s = t - (h * 3600 + m * 60)
+
+print('{}:{}:{}'.format(h, m, s))
