d7c32f81bce3f115d847a3c4df3ee2e5afa72f65
Alexsandro Felix <felix@ffelix.eti.br>
2019-05-14 08:27:46 -0300

    teste de git no vscode

--- 01-iniciante/1037-intervalo.py
+++ 01-iniciante/1037-intervalo.py
@@ -1,4 +1,5 @@
 '''
+by Alexsandro Felix
 Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem
  dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100])
   este valor se encontra. Obviamente se o valor não estiver em nenhum destes
